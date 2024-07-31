8307c06174a4df4f0fe78917c3b89703a47f51a7
Reem Joudeh <joudehreem@gmail.com>
2024-07-31 14:23:44 +0300

    uplode template

--- app_labology/urls.py
+++ app_labology/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('1', views.error),
+    path('2', views.about),
+    path('3', views.all_patients),
+    path('4', views.all_tests),
+    path('5', views.patient),
+    path('6', views.index),
+    path('7', views.new_patient),
+
 ]
+
+
+#logo - error{{sweet alert}}- footer in main only and about
--- app_labology/views.py
+++ app_labology/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def error(request):
+  return render(request,'404.html')
+def about(request):
+  return render(request,'about.html')
+def all_patients(request):
+  return render(request,'all_patients.html')
+def all_tests(request):
+  return render(request,'all_tests.html')
+def patient(request):
+  return render(request,'patient.html')
 def index(request):
+  return render(request,'login.html')
+def new_patient(request):
   return render(request,'new_patient.html')
